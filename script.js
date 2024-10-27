9767e504a82c50b4c6a85aacb7be9bfd2a2e9d62
ddblock212 <korobchuk.tima@gmail.com>
2024-10-27 20:32:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,11 @@
 function showSection(sectionId) {
-    // Отримати всі секції
     const sections = document.querySelectorAll('.section');
-    
-    // Перебрати секції та приховати всі, окрім обраної
     sections.forEach(section => {
         section.style.display = 'none';
     });
-    
-    // Показати обрану секцію
     document.getElementById(sectionId).style.display = 'block';
 }
 
-// Повідомлення при надсиланні форми
 document.getElementById('reportForm').addEventListener('submit', function(event) {
     event.preventDefault();
     alert('Ваше повідомлення відправлено! Поліція Негровки реагуватиме негайно.');
